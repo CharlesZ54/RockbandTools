49cddd4fab2c19765741e79050bda0e251e8edf9
Charles Zwicker <czwicker54@gmail.com>
2019-11-21 19:07:02 -0500

    First iteration of findDuplicates function complete

--- GUI_SongList.py
+++ GUI_SongList.py
@@ -6,12 +6,40 @@ import tkinter.scrolledtext as tkst
 window = Tk()
 window.title("Rockband Song List")
 
-file_path = ""
 var = IntVar()
 var.set(0)
 
-def display():
-    list = listsongs()
+def rename(file_path):
+    #num_folders = len(os.walk(file_path).next()[1])
+    #folder_num = raw_input("What number should I start with? ")
+    folder_num = 56
+
+    for i,filename in enumerate(os.listdir(file_path)):
+        if filename.startswith("000_"):
+            new_filename = str(folder_num).zfill(3) + "_" + hex(folder_num).split('x')[-1].zfill(8) + filename[12:]
+            os.rename(filename, new_filename)
+            text_str = "sZFE/" + str(folder_num).zfill(3)
+            folder_num += 1
+
+        if filename.endswith("_meta"):
+            f1 = open(new_filename + "/content/songs/songs.dta","r")
+            f2 = open(new_filename + "/content/songs/songs_new.dta","w")
+            for line in f1:
+                f2.write(line.replace("sZAE/000",text_str))
+            f1.close()
+            f2.close()
+            os.remove(new_filename + "/content/songs/songs.dta")
+            os.rename(new_filename + "/content/songs/songs_new.dta",new_filename + "/content/songs/songs.dta")
+
+def findDuplicates(list):
+    names = {}
+    for song in list:
+        if song[0] in names:
+            print("Found Duplicate of: " + song[0])
+        names[song[0]] = song[1]
+
+def display(file_path):
+    list = listSongs(file_path)
     messages.config(state=NORMAL)
     messages.delete(1.0,END)
     for song in list:
@@ -23,7 +51,7 @@ def display():
 
     messages.config(state=DISABLED)
 
-def listsongs():
+def listSongs(file_path):
     song_list = []
     pattern_meta = re.compile(".{3}_.{8}_.*_meta")
     for i,filename in enumerate(os.listdir(file_path)):
@@ -39,16 +67,24 @@ def selectfolder():
     global file_path
     file_path = filedialog.askdirectory()+"/"
     btn.configure(text=file_path)
+    display(file_path)
+
+# file_path='/home/charles/Documents/RockbandTools/SZFE/'
+# findDuplicates(listSongs())
+
 
 #Parent widget for buttons
 btn = Button(window, text="Select a folder", command=selectfolder)
 btn.grid(column=0, row=0)
 
-btn2 = Button(window, text="List songs", command=display)
+btn2 = Button(window, text="Refresh", command= lambda: display(file_path))
 btn2.grid(column=1, row=0)
 
 box = Checkbutton(window, text="Export to CSV", variable=var)
 box.grid(column=2, row=0, padx=10, pady=10)
+
+btn3 = Button(window, text="Rename", command= lambda:rename(file_path))
+btn3.grid(column=0, row=1)
 
 #Group1 Frame
 window.columnconfigure(0, weight=1)
@@ -56,6 +92,6 @@ window.rowconfigure(1, weight=1)
 
 # Textbox
 messages = tkst.ScrolledText(window, width=60, height=30)
-messages.grid(row=1,column=0, sticky = E+W+N+S, columnspan=3)
+messages.grid(row=2,column=0, sticky = E+W+N+S, columnspan=3)
 
 mainloop()
