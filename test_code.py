b1e4fdc524818d19a2e8a06f1fb097e85c511fbb
Charles Zwicker <czwicker54@gmail.com>
2019-12-02 13:14:10 -0500

    Moved pytest file directory

--- test_code.py
+++ test_code.py
@@ -5,26 +5,26 @@ import shutil
 
 def test_findDuplicates():
     expected = ['Hotel California']
-    result = findDuplicates(listSongs("../sZFE/"))
+    result = findDuplicates(listSongs("./sZFE/"))
     assert result == expected
 
 def test_display():
     expected = ['Goodbye Blue Sky', 'Hotel California', 'Hotel California', 'Fluttering Romance', 'Born to Run']
-    result = display("../sZFE/")
+    result = display("./sZFE/")
     assert result == expected
 
 def test_listSongs():
     expected = [['Goodbye Blue Sky', 'Pink Floyd', 'The Wall'], ['Hotel California', 'The Eagles', 'Hotel California'], ['Hotel California', 'The Eagles', 'Hotel California'], ['Fluttering Romance', 'Nightmare Lyre', 'The Rainbow Colored Album'], ['Born to Run', 'Bruce Springsteen', 'Born To Run']]
-    result = listSongs("../sZFE/")
+    result = listSongs("./sZFE/")
     assert result == expected
 
 def test_exportCSV():
-    exportCSV("../sZFE/")
+    exportCSV("./sZFE/")
     result = os.path.isfile(os.getcwd() + "/rockband_songs.csv")
     assert result == TRUE
 
 def test_copySongs():
-    copySongs("../sZFE/",os.getcwd())
+    copySongs("./sZFE/",os.getcwd())
     assert os.path.isdir("export") == TRUE
     assert os.path.isdir("export/052_00000034_nlfr_meta") == TRUE
 
